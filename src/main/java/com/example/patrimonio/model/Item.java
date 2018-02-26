a282796d93cbaa3feae4016a77d2b06440a27784
Vitor Lima <vitor.lima@msn.com>
2018-02-25 22:04:25 -0300

    Create POST method and insert 2 itens in database through the import.sql

--- src/main/java/com/example/patrimonio/model/Item.java
+++ src/main/java/com/example/patrimonio/model/Item.java
@@ -21,31 +21,6 @@ public class Item {
 
     private LocalDate dataAquisicao;
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Item other = (Item) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
-
     public Long getId() {
         return id;
     }
@@ -76,5 +51,30 @@ public class Item {
 
     public void setDataAquisicao(LocalDate dataAquisicao) {
         this.dataAquisicao = dataAquisicao;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Item other = (Item) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 }
--- src/main/java/com/example/patrimonio/resource/ItemResource.java
+++ src/main/java/com/example/patrimonio/resource/ItemResource.java
@@ -2,22 +2,33 @@ package com.example.patrimonio.resource;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.patrimonio.model.Item;
 import com.example.patrimonio.repository.ItemRepository;
 
 @RestController
+@CrossOrigin("${permission}")
 public class ItemResource {
 
     @Autowired
     private ItemRepository itemRepository;
 
     @GetMapping("/item")
-    public List<Item> listar(){
+    public List<Item> listar() {
         return itemRepository.findAll();
+    }
+
+    @PostMapping("/item")
+    public Item adicionar(@RequestBody @Valid Item item) {
+        return itemRepository.save(item);
     }
 
 }
